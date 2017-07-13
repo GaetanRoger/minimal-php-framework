568bc8ae5ae6a139e72d91e72c9ff13a1d938a6b
Gaétan ROGER <gaetanroger.fr@gmail.com>
2017-07-13 11:33:17 +0200

    Create manager class

---
+++ src/Manager.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace GrBaseFramework;
+
+
+/**
+ * Class Manager
+ *
+ * @author Gaetan
+ * @date   13/07/2017
+ */
+abstract class Manager
+{
+    /**
+     * Database instance.
+     *
+     * @var \PDO $database
+     */
+    public static $database;
+}
