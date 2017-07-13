bf12f55b5ce1dc1ad9fa0708c8602dd5f3baf31a
Gaétan ROGER <gaetanroger.fr@gmail.com>
2017-07-13 12:05:40 +0200

    Adds getTableName() to manager

--- src/Manager.php
+++ src/Manager.php
@@ -18,4 +18,26 @@ abstract class Manager
      * @var \PDO $database
      */
     public static $database;
+    
+    
+    /**
+     * Return manager table name.
+     *
+     * For this to work, the following rules **must** be followed :
+     * * A manager class name **must** use CamelCase;
+     * * A manager class name **must** end with `Manager`;
+     * * A model class name **must** not contain `Manager` anywhere else;
+     * * The table name **must** use snake_case;
+     * * The manager class name **must** be the exact conversion from snake_case to CamelCase of the table name.
+     *
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        $reflectionClass = new \ReflectionClass(static::class);
+        $className = $reflectionClass->getShortName();
+        $name = str_replace('Manager', '', $className);
+    
+        return Utils::camelCaseToSnakeCase($name);
+    }
 }
