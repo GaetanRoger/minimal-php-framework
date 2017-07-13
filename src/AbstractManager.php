50c2058047bd2036f41b3aac2420e86bbd329a1a
Gaétan ROGER <gaetanroger.fr@gmail.com>
2017-07-13 19:35:52 +0200

    Commenting AbstractManager in detail

--- src/AbstractManager.php
+++ src/AbstractManager.php
@@ -7,6 +7,8 @@ namespace GrBaseFramework;
 /**
  * Class Manager
  *
+ * A manager stores all static methods not specific to one model.
+ *
  * @author Gaetan
  * @date   13/07/2017
  */
@@ -20,21 +22,29 @@ abstract class AbstractManager
     public static $database;
     
     /**
-     * @param int $id
-     * @return AbstractModel
-     * @throws \InvalidArgumentException If nothing found.
+     * Find the model associated with the given ID.
+     *
+     * @param   int $id ID of the model to find.
+     * @return  AbstractModel
+     * @throws  \InvalidArgumentException If nothing was found.
      */
     public static function find(int $id): AbstractModel
     {
+        // Finding all models matching the ID (there should be only one)
         $results = static::findWhere(['id' => $id]);
         
-        if (empty($results))
+        // Throwing an exception if none was found
+        if (empty($results)) {
             throw new \InvalidArgumentException("Model not found with id $id.");
+        }
         
+        // Returning the model
         return $results[0];
     }
     
     /**
+     * Find all models.
+     *
      * @return AbstractModel[]
      */
     public static function findAll(): array
@@ -43,38 +53,59 @@ abstract class AbstractManager
     }
     
     /**
-     * @param array $conditions
-     * @return AbstractModel[]
+     * Find models matching conditions.
+     *
+     * All the conditions are strict equality checks (`=` sign).
+     *
+     * @param array $conditions Associative array with key being the column and value being the value to test.
+     * @return AbstractModel[] Found models.
      */
     public static function findWhere(array $conditions): array
     {
+        // Counting conditions
         $conditionsCount = count($conditions);
-    
+        
+        // If no conditions given, perform a simple SELECT * on the table
         if ($conditionsCount === 0) {
             return
                 static::$database->query('SELECT * FROM ' . static::getTableName())
-                ->fetchAll(\PDO::FETCH_CLASS, self::getModelName(false));
+                    ->fetchAll(\PDO::FETCH_CLASS, self::getModelName(false));
         }
         
-        
+        // WHERE conditions
         $where = ' WHERE';
+        
+        // Array of columns
         $columns = array_keys($conditions);
+        
+        // Array of values
         $values = array_values($conditions);
         
+        
+        // Foreach condition
         for ($i = 0; $i < $conditionsCount; ++$i) {
+            
+            // Appending it to the where string
             $where .= ' ' . $columns[$i] . ' = :' . $columns[$i];
             
-            if ($i < $conditionsCount - 1)
+            // Adding AND keyword at the end
+            if ($i < $conditionsCount - 1) {
                 $where .= ' AND';
+            }
         }
         
+        
+        // Preparing query
         $statement = static::$database->prepare('SELECT * FROM ' . static::getTableName() . $where);
-    
+        
+        // Binding values
         for ($i = 0; $i < $conditionsCount; ++$i) {
             $statement->bindValue(':' . $columns[$i], $values[$i]);
         }
         
+        // Running query
         $statement->execute();
+        
         return $statement->fetchAll(\PDO::FETCH_CLASS, static::getModelName(false));
     }
     
@@ -91,10 +122,14 @@ abstract class AbstractManager
      */
     public static function getTableName(): string
     {
+        // Getting class name
         $reflectionClass = new \ReflectionClass(static::class);
         $className = $reflectionClass->getShortName();
+        
+        // Removing `Manager` keyword
         $name = str_replace('Manager', '', $className);
         
+        // Converting to snake_case
         return Utils::camelCaseToSnakeCase($name);
     }
     
@@ -110,12 +145,19 @@ abstract class AbstractManager
      */
     public static function getModelName(bool $short = true): string
     {
+        // Getting class name
         $reflectionClass = new \ReflectionClass(static::class);
         $className = $reflectionClass->getShortName();
-        $shortName =  str_replace('Manager', '', $className);
         
-        if ($short) return $shortName;
+        // Removing `Manager` keyword
+        $shortName = str_replace('Manager', '', $className);
         
+        // If short, returning the name
+        if ($short) {
+            return $shortName;
+        }
+        
+        // Else adding namespace
         return $reflectionClass->getNamespaceName() . '\\' . $shortName;
     }
 }
